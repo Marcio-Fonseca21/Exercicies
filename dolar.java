b3f3218808a043ea606a5775b185ae3fe86bf179
VictorOlima <victor.henrique.3t@gmail.com>
2024-02-18 20:58:20 -0400

    Cotação dolar para Real

---
+++ dolar.java
@@ -0,0 +1,40 @@
+/*************************************************************************
+ *    Faça um programa que: 
+ *  - Leia a cotação do dólar 
+ *  - Leia um valor em dólares
+ *  - Converta esse valor para Real 
+ *  - Mostre o resultado 
+ *************************************************************************/
+
+import java.util.Scanner;
+
+/**
+ * dolar
+ */
+public class dolar {
+
+    
+    public static void main(String[] args) {
+        
+
+        Scanner entrada = new Scanner(System.in);
+
+        double cotacao,dolar,convert,soma;
+        
+        System.out.println("Conversor dolar para real");
+
+        System.out.println("Digite o valor do dolar");
+        cotacao = entrada.nextDouble();
+
+        System.out.printf("Digite um valor em Dolar ");
+        dolar = entrada.nextDouble();
+
+        soma = cotacao * dolar;
+        convert = soma * 1;
+
+        System.out.printf("O valor em real é: %.2f", convert);
+
+        entrada.close();
+    }
+
+}
