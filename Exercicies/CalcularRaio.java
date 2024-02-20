2c7ac374cd4bb971d8cce75c76b3dac5fe66494e
VictorOlima <victor.henrique.3t@gmail.com>
2024-02-19 21:42:45 -0400

    Calcular Raio

---
+++ Exercicies/CalcularRaio.java
@@ -0,0 +1,38 @@
+package Exercicies;
+/*****************************************************************************************************************
+* 8) Calcular e apresentar o valor do volume de uma lata de óleo, utilizando a fórmula:
+* 		V = 3.14159 * R * R * A
+* 		Onde as variáveis: V, R e A representam respectivamente o volume, o raio e a altura.
+*****************************************************************************************************************/
+
+import java.util.Scanner;
+
+/**
+ * CacularRaio
+ */
+public class CalcularRaio {
+
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+
+        double V,R,A;
+
+        System.out.printf("Calcular valor do volume de uma lata de óleo\n\n");
+
+        System.out.println("Digite o valor da Altura: ");
+        A = entrada.nextDouble();
+
+        System.out.println("Digite o valor do raio: ");
+        R = entrada.nextDouble();
+        // math.pi e uma constante aproximada de pi = 3.14159
+        V = Math.PI * R * R * A;
+
+        System.out.printf("O Valor do volume da lata é : %.2f", V);
+
+        entrada.close();
+
+    }
+
+
+}
